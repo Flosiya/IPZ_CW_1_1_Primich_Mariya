da02922992e6f534fab15043ccae3b1c69ffd084
Flosiya <mashameky@gmail.com>
2024-03-11 13:48:55 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IPZ_CW_1_1_Primich_Mariya"
+include(":app")
+ 
